bf13dbd2e215e5209ee8fc5b6a66c84e596991d1
Mwangi Muriuki <muriukimwangi.mm@gmail.com>
2020-02-18 10:49:32 +0300

    Commit 2. Final app

--- 8 Ball/ViewController.swift
+++ 8 Ball/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var balImageView: UIImageView!
+    
+    var randomValue = 0
+    
+    let ansArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateBallFaces()
+        
+    }
+    
+    func updateBallFaces(){
+        
+        randomValue = Int(arc4random_uniform(5))
+        balImageView.image = UIImage(named: ansArray[randomValue])
+        
     }
 
-
+    @IBAction func askButton(_ sender: Any) {
+        updateBallFaces()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateBallFaces()
+    }
 }
 
